3d9752b95e7c92e83d30c9236c144f164f426518
Дмитрий Лёвкин <jonikm892@gmail.com>
2025-03-25 21:37:24 +0300

    Модель BookNode

--- LibraryApp/LibraryApp/Модели/BookNode.cs
+++ LibraryApp/LibraryApp/Модели/BookNode.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace LibraryApp.Модели
 {
-    class BookNode
+    /// <summary>
+    /// Класс, представляющий узел двусвязного списка
+    /// Каждый узел содержит данные о книге и ссылки на соседние узлы
+    /// </summary>
+    public class BookNode
     {
+        /// <summary>
+        /// Конструктор узла
+        /// </summary>
+        /// <param name="book">Книга, которая будет храниться в этом узле</param>
+        public BookNode(Book data, BookNode next, BookNode previous)
+        {
+            Data = data;
+            Next = next;
+            Previous = previous;
+        }
+
+        public Book Data { get; set; } // Данные о книге, хранящиеся в этом узле
+        public BookNode Next { get; set; } // Ссылка на следующий узел в списке
+        public BookNode Previous { get; set; } // Ссылка на предыдущий узел в списке
+
     }
 }
