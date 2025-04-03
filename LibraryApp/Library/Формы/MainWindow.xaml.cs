be1f0531b4fb5092b5e4b86f80b570e22646e686
Дмитрий Лёвкин <jonikm892@gmail.com>
2025-04-03 14:38:58 +0300

    Исправил ошибку с пространством имен

--- LibraryApp/Library/Формы/MainWindow.xaml.cs
+++ LibraryApp/Library/Формы/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Library
+namespace Library.Формы
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
